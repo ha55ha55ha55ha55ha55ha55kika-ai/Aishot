fd04bae93f2f07b936c87020fe050aa2996a731f
ha55ha55ha55ha55ha55ha55kika-ai <ha55ha55ha55ha55ha55ha55kika@gmail.com>
2026-08-13 10:28:47 +0300

    Remove allprojects block from build.gradle.kts
    Removed allprojects block and its repositories configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,3 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     kotlin("android") version "1.9.22" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
